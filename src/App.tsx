ecd54e3c89c32794dceb0eaadf74d7a9c7be8922
Ouifi <ouifidev@gmail.com>
2022-05-10 21:23:08 -0400

    Added auth, login, logout to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Example1 } from 'components/Example1';
+import { AuthContext, AuthProvider } from 'context/authContext';
+
+const Protector = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, attemptSignIn, triggerSignOut } = useContext(AuthContext);
+
+  return <>
+    {
+      isAuthenticated
+        ? <>
+          {children}
+          <button style={{ backgroundColor: "yellow", color: "#61dafb", padding: 10, fontSize: "larger", borderRadius: "5px", border: "none" }} onClick={() => triggerSignOut()}>
+            Log Out
+          </button>
+        </>
+        : <>
+          <span>Click the button to see cool Chuck Norris facts!</span>
+          <button style={{ backgroundColor: "#61dafb", color: "yellow", padding: 10, fontSize: "larger", borderRadius: "5px", border: "none" }} onClick={() => attemptSignIn()}>
+            Log In
+          </button>
+        </>
+    }
+  </>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Example1 />
-      </header>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <header className="App-header">
+          <Protector>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Example1 />
+          </Protector>
+        </header>
+      </div>
+    </AuthProvider>
   );
 }
 
